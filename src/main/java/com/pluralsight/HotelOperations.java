8097eb6eb333acc7686c6c8afcc6e5851fdf24d5
Stephen_A <ankustephen99@gmail.com>
2025-10-20 13:45:27 -0400

    Created Room.java class getters and constructer

--- src/main/java/com/pluralsight/HotelOperations.java
+++ src/main/java/com/pluralsight/HotelOperations.java
@@ -3,5 +3,9 @@ package com.pluralsight;
 public class HotelOperations {
     public static void main(String[] args) {
 
+        Room newRoom = new Room(3, 50, false, false);
+
+        newRoom.setOccupied(true);
+        System.out.println(newRoom.isAvailable);
     }
 }
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,5 +1,42 @@
 package com.pluralsight;
 
 public class Room {
+    int numberOfBeds;
+    double price;
+    boolean isOccupied;
+    boolean isDirty;
+    boolean isAvailable;
 
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+        this.isAvailable = (!this.isDirty && !this.isOccupied);
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
 }
+
