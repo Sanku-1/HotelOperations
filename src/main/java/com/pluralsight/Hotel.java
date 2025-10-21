d448df6321ba5ac244474afefd03df600a36d433
Stephen_A <ankustephen99@gmail.com>
2025-10-21 11:58:31 -0400

    Created Hotel.java class and required constructors

---
+++ src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Hotel {
+    String name;
+    int numberOfSuites;
+    int numberOfRooms;
+    int bookedSuites;
+    int bookedBasicRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+    }
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+}
