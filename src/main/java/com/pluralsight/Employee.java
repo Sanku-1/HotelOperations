6de01141d9eae4b51e535daed50354f0f64aa9df
Stephen_A <ankustephen99@gmail.com>
2025-10-21 11:51:40 -0400

    Added punchTimeClock method that works off of automatically logged LocalDateTime Instance instead of input times

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+
 public class Employee {
     String employeeId;
     String name;
@@ -11,6 +13,9 @@ public class Employee {
     double overtimeHours;
     double punchInTime;
     double punchOutTime;
+    LocalDateTime punchInLocalDateTime;
+    LocalDateTime punchOutLocalDateTime;
+
 
     Employee(String employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
@@ -71,6 +76,23 @@ public class Employee {
             this.hoursWorked += hoursWorkedThisShift;
             this.punchInTime = 0;
             this.punchOutTime = 0;
+        }
+    }
+
+    public void punchTimeClock() {
+        if (this.punchInLocalDateTime == null) {
+            this.punchInLocalDateTime = LocalDateTime.now();
+        } else {
+            this.punchInLocalDateTime = LocalDateTime.now();
+            double punchInHours = punchInLocalDateTime.getHour();
+            double punchInMinutes = punchInLocalDateTime.getMinute();
+            double punchOutHours = punchOutLocalDateTime.getHour();
+            double punchOutMinutes = punchOutLocalDateTime.getMinute();
+            double punchInTimeInHours = punchInHours + (punchInMinutes/60);
+            double punchOutTimeInHours = punchOutHours + (punchOutMinutes/60);
+            double hoursWorkedThisShift = punchOutTimeInHours - punchInTimeInHours;
+            this.hoursWorked += hoursWorkedThisShift;
+            this.punchInLocalDateTime = null;
         }
     }
 }
