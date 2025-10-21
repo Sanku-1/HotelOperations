675e091762ae076b2b020832286c4ec594395c06
Stephen_A <ankustephen99@gmail.com>
2025-10-21 12:10:12 -0400

    Created bookRoom() method in Hotel.java class, allowing guests to book rooms as long as the specified type is available, and updates the availability of said rooms after the fact to reflect any changes

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -12,11 +12,32 @@ public class Hotel {
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
     }
+
     public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
         this.name = name;
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
         this.bookedSuites = bookedSuites;
         this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        boolean booked = false;
+        if (isSuite) {
+            if (numberOfRooms <= (this.numberOfSuites - this.bookedSuites)) {
+                this.bookedSuites += numberOfRooms;
+                booked = true;
+            } else {
+                System.out.println("Unfortunately there aren't enough available rooms for your request");
+            }
+        } else {
+            if (numberOfRooms <= (this.numberOfRooms - this.bookedBasicRooms)) {
+                this.bookedBasicRooms += numberOfRooms;
+                booked = true;
+            } else {
+                System.out.println("Unfortunately there aren't enough available rooms for your request");
+            }
+        }
+        return booked;
     }
 }
